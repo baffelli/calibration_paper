0472354d0ebc1af9146c898df2384de4e65b51e7
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-09-30 13:45:56 +0000

    Added scripts to generate each figure separately instead of using snakemake wildcards to generate them all

--- Snakefile
+++ Snakefile
@@ -10,22 +10,45 @@ import json
 
 
 
+
 subworkflow old_data:
-    snakefile: './analyze_old_data.snake'
+    workdir:    './processed'
+    snakefile:  pyrat.rules['slc_to_calibrated_c']
     configfile: './calibration_configuration_20160222.json'
 
 subworkflow new_data:
-    snakefile: './calibration_analysis.snake'
+    workdir:    './processed'
+    snakefile:  pyrat.rules['slc_to_calibrated_c']
     configfile: './calibration_configuration_chutze.json'
 
 rule all:
     input:
-        old_data('outputs/img/HV_gain.pdf'),
-#        old_data('outputs/img/HV_loss.pdf'),
-        new_data('analysis.done'),
-#        'doc/calibration_paper.pdf'
+        './fig/figure_1.pdf'
+#        old_data('outputs/img/HV_gain.pdf'),
+##        old_data('outputs/img/HV_loss.pdf'),
+#        new_data('analysis.done'),
+##        'doc/calibration_paper.pdf'
 
 
+
+###############################################################################
+
+rule fig1:
+    output:
+        './fig/figure_1.pdf'
+    input:
+        HH = new_data('slc_chan/20160914_145059_AAAl.slc'),
+        VV = new_data('slc_chan/20160914_145059_BBBl.slc'),
+        HH_desq = new_data('slc_desq/20160914_145059_AAAl.slc'),
+        VV_desq = new_data('slc_desq/20160914_145059_BBBl.slc'),
+        HH_corr = new_data('slc_corr/20160914_145059_AAAl.slc'),
+        VV_corr = new_data('slc_corr/20160914_145059_BBBl.slc'),
+        style = 'paper_style.rc'
+    params:
+        ridx = 1331,
+        azidx = 1490
+    script:
+        './scripts/figure_1.py'
 
 
 
---
+++ scripts/figure_1.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pyrat.core.corefun as cf
+import pyrat.fileutils.gpri_files as gpf
+import pyrat.visualization.visfun as vf
+
+
+def plot_figure_1(inputs, outputs, threads, config, params, wildcards):
+    plt.style.use(inputs['style'])
+    # This is a full page figure, so we create a figure twice as wide as usual
+    # create figure
+    fig_w, fig_h = plt.rcParams['figure.figsize']
+    f, axarr = plt.subplots(2, 3, figsize=(fig_w * 3, fig_h * 2))
+    for idx_chan, chan_str in enumerate(['HH', 'VV']):
+        for idx_proc, proc_str in enumerate(['', '_desq', '_corr']):
+            current_ax = axarr[idx_chan, idx_proc]
+            file_name = inputs[chan_str + proc_str]
+            current_slc = gpf.gammaDataset(file_name + '.par', file_name)  # load slc
+            ptarg_zoom, r_plot, az_plot, mx_pos, res_dict, r_vec, az_vec = cf.ptarg(current_slc, params['ridx'],
+                                                                                    params['azidx'],
+                                                                                    azwin=128, rwin=20)
+            mph, rgb, norm = vf.dismph(ptarg_zoom, k=0.4, sf=0.1, coherence=False)  # create rgb image
+            mappable = current_ax.imshow(mph, cmap=rgb, extent=[az_vec.min(), az_vec.max(), r_vec.min(), r_vec.max()],
+                                         aspect=1 / 10.0)
+            # add resolution analysis
+            resolution_text = r"\begin{{tabular}}{{l}}Range resolution: {r_res:.3f} m\\Azimuth resolution:"
+            " {az_res:.3f}$^\circ$\end{{tabular}}".format(r_res=res_dict['range_resolution'][0],
+                                                          az_res=res_dict['azimuth_resolution'][
+                                                              0])
+            current_ax.text(0.7, 0.1, resolution_text, size=5)#set label
+        if idx_chan == 1 and idx_proc == 0:
+            current_ax.set_xlabel(r'azimuth samples $\theta$ [deg]')
+            current_ax.set_ylabel(r'range samples [m]')
+
+    ticks = [0, 0.25, 0.5, 0.75, 1]
+    labels = ['-180', '-90', '0', '90', '180']
+    # Make space for colorbar
+    # f.subplots_adjust(right=0.8)
+    # cbar_ax = divider.append_axes("left", size="20%", pad=0.25)
+    cax = f.add_axes([0.1, 0.02, 0.5, 0.015])
+    cbar = f.colorbar(mappable, label=r'Phase [deg]', orientation='horizontal', ticks=ticks, cax=cax)
+    cbar.set_ticks(ticks)
+    cbar.set_ticklabels(labels)
+    cbar.update_ticks()
+    f.savefig(outputs[0])
+
+
+plot_figure_1(snakemake.input, snakemake.output, snakemake.threads, snakemake.config, snakemake.params,
+              snakemake.wildcards)
