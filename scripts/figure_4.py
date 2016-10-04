d9cab8ceb471ae2608632c7d08c42883b83bd3d7
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-10-04 07:52:12 +0000

    Added script to generate figure 4: plotting of HV power before and after shift correction.

---
+++ scripts/figure_4.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pyrat.fileutils.gpri_files as gpf
+import pyrat.core.corefun as cf
+import pyrat.core.polfun as  pf
+
+def plot_figure_4(inputs, outputs, threads, config, params, wildcards):
+    plt.style.use(inputs['style'])
+    HV = gpf.gammaDataset(inputs.C_HV_old + '.par', inputs.C_HV_old)
+    HV_corr = gpf.gammaDataset(inputs.C_HV_new + '.par', inputs.C_HV_new)
+    ridx = params.ridx
+    azidx = params.azidx
+    sw = (2, 10)
+    ptarg_zoom, rplot, azplot, mx_idx_zoom, res_dict, r_vec, az_vec = cf.ptarg(HV, ridx, azidx, sw=sw, azwin=30)
+    ptarg_zoom_corr, rplot_corr, azplot_corr, mx_idx_zoom_corr, res_dict, r_vec_corr, az_vec_corr = cf.ptarg(HV_corr, ridx, azidx, sw=sw,
+                                                                                    azwin=30)
+    HV_gain = cf.dB(ptarg_zoom_corr[mx_idx_zoom_corr] / ptarg_zoom[mx_idx_zoom])
+    x_shift = 50
+    f, ax = plt.subplots()
+    norm_plot = plt.plot(az_vec, cf.dB(np.abs(azplot)), label='No shift')
+    corr_plot = plt.plot(az_vec_corr, cf.dB(np.abs(azplot_corr)), label='Optimal shift')
+    plt.axhline(cf.dB(np.abs(ptarg_zoom[mx_idx_zoom])), color=norm_plot[0].get_color(), linestyle='--')
+    plt.axhline(cf.dB(np.abs(ptarg_zoom_corr[mx_idx_zoom_corr])), color=corr_plot[0].get_color(), linestyle='--')
+    ax.arrow(az_vec[mx_idx_zoom[1] + x_shift], cf.dB(np.abs(ptarg_zoom[mx_idx_zoom])), 0, HV_gain)
+    ax.text(az_vec[mx_idx_zoom[1] + x_shift + 5], cf.dB(np.abs(ptarg_zoom[mx_idx_zoom])) + HV_gain / 2,
+             "gain: {HV:.2f} dB".format(HV=HV_gain))
+    ax.xaxis.set_label_text(r'azimuth samples')
+    ax.yaxis.set_label_text(r'HV power [dB]')
+    ax.set_ylim([0, 35])
+    ax.grid(True)
+    plt.legend(loc='lower left')
+    f.subplots_adjust(bottom=0.15)
+    f.savefig(outputs[0], pad_inches=0.1)
+
+plot_figure_4(snakemake.input, snakemake.output, snakemake.threads, snakemake.config, snakemake.params,
+              snakemake.wildcards)
