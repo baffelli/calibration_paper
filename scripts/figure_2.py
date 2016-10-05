6ffe1086cf31d8a4f899597884da830a2025f11d
Simone Baffelli <baffelli@ifu.baug.ethz.ch>
2016-10-05 16:06:54 +0000

    Figures 1 to 4 complete, figure 5 needs some improvement

--- scripts/figure_2.py
+++ scripts/figure_2.py
@@ -4,7 +4,6 @@ import numpy as np
 import pyrat.core.corefun as cf
 import pyrat.fileutils.gpri_files as gpf
 import pyrat.visualization.visfun as vf
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Window for plotting in degress
 ws = 1.2
@@ -20,9 +19,9 @@ def plot_figure_2(inputs, outputs, threads, config, params, wildcards):
     cm = mpl.cm.get_cmap('viridis', len(sorted_by_range))  # colormap for sorting
     ridx_vec = [el[0] for el in sorted_by_range]
     ranges = slc.r_vec[ridx_vec]
-    mappable = mpl.cm.ScalarMappable(cmap=cm, norm=mpl.colors.Normalize(vmin=ranges[0], vmax=ranges[-1]))
-    mappable.set_array(ranges)#set the array of range distances to the mappable
-    cbar_ticks = [r"\tiny{{r={r_sl:.2f}~m}}".format(r_sl=r_sl) for r_sl in ranges]
+    nm = mpl.colors.Normalize(vmin=ranges[0], vmax=ranges[-1])
+    mappable = mpl.cm.ScalarMappable(cmap=cm, norm=nm)
+    mappable.set_array(ranges)  # set the array of range distances to the mappable
     for ridx, azidx, *rest in sorted_by_range:
         ws_id = ws / slc.GPRI_az_angle_step[0]  # number of samples in degrees
         # Extract maximum around the supposed position
@@ -39,7 +38,8 @@ def plot_figure_2(inputs, outputs, threads, config, params, wildcards):
         refl_amp = (np.abs(reflector_slice)) ** 2
         r_sl = slc.r_vec[ridx]
         line, = phase_ax.plot(az_vec, np.rad2deg(refl_ph), color=mappable.to_rgba(r_sl))
-        amp_ax.plot(az_vec, refl_amp / refl_amp[reflector_slice.shape[0] / 2], color=mappable.to_rgba(r_sl),)
+        amp_ax.plot(az_vec, refl_amp / refl_amp[reflector_slice.shape[0] / 2], color=mappable.to_rgba(r_sl),
+                    label=r"{r_sl:.0f}".format(r_sl=r_sl))
         # Plot line for beamwidth
         phase_ax.set_ylim(-30, 30)
         lc = "#cc8e8e"
@@ -53,9 +53,9 @@ def plot_figure_2(inputs, outputs, threads, config, params, wildcards):
         amp_ax.xaxis.set_label_text(r'azimuth angle from maximum [deg]')
         # Format axes
         map(vf.format_axes, f.get_axes())
-    cax = f.add_axes([0.25, 0.0, 0.5, 0.04])
-    cax = f.colorbar(mappable, orientation='horizontal', cax=cax, bounds=len(sorted_by_range)+1)
-    # cax.ax.set_xticklabels(cbar_ticks)
+    leg = amp_ax.legend(loc='lower center', ncol=len(ranges) // 2, fancybox=True, frameon=True, shadow=False, framealpha=None,
+                  title=r"Distance from radar [m]", handlelength=0.3)
+    leg.get_title().set_fontsize(plt.rcParams['legend.fontsize'])
     f.subplots_adjust(top=1)
     f.savefig(outputs[0])
     plt.close(f)
