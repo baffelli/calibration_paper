8ef8b640ee9d3aaa773d13bbf052bff957209ef5
baffelli <baffelli@ifu.baug.ethz.ch>
2017-02-07 12:06:24 +0100

    Refactored scripts to compute squint by moving the squint estimation function to pyrat.

--- scripts/figure_12.py
+++ scripts/figure_12.py
@@ -1,108 +1,97 @@
+import string
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tick
 import numpy as np
-import pyrat.core.corefun as cf
-import pyrat.diff.intfun as _ifun
-import pyrat.fileutils.gpri_files as gpf
-import pyrat.visualization.visfun as vf
 from matplotlib import gridspec
-import matplotlib.ticker as tick
-import string
-import scipy.signal as _sig
+
+import pyrat.fileutils.gpri_files as gpf
+from pyrat.gpri_utils import calibration as cal
 
 
-# Return the decimated azimuth position
 def az_idx(ds, idx):
     return idx / ds.azimuth_looks
 
+
 def center_width_from_slice(sl):
-    center = (sl.stop + sl.start)//2
+    center = (sl.stop + sl.start) // 2
     width = (sl.stop - sl.start)
     return center, width
 
 
-def squint_vec(rawdata, z=2500):
-    win_slice = slice(z, rawdata.shape[0] - z)
-    rawdata_sl = rawdata[win_slice,:]#window the edges
-    max_idx = np.argmax(np.abs(rawdata_sl), axis=1)
-    return max_idx, win_slice
-
-
-width = (10, 150)
-shp = (2,2)
-#range window for filter in pizzels
-rwin = 4
-
+width = (6, 150)
+shp = (2, 2)
+# range window for filter in pizzels
+# Pixels to discard at beginning and end of chirp
+z = 2000
 xlabel = r' $\theta_{sq}$ (Azimuth rel. to pointing at $f_c$) $[^\circ]$'
 ylabel = r'Chirp frequency $f[MHz]$'
 
 
 def format_freq(raw_par, x, pos):
-    rel_freq = (x - raw_par.RF_center_freq) / 1e6#relative frequency
+    rel_freq = (x - raw_par.RF_center_freq) / 1e6  # relative frequency
     sign = '-' if rel_freq < 0 else '+'
-    lab = r'$f_c$ {sign:^10} {rel_freq:^1.0f}'.format(sign= sign, rel_freq=np.abs(rel_freq))
+    lab = r'$f_c$ {sign:^10} {rel_freq:^1.0f}'.format(sign=sign, rel_freq=np.abs(rel_freq))
     return lab
+
 
 def plot_figure_12(inputs, outputs, threads, config, params, wildcards):
     # Create figure
     plt.style.use(inputs['style'])
     fig_w, fig_h = plt.rcParams['figure.figsize']
-    f = plt.figure(figsize=(fig_w*2, fig_h*2))
+    f = plt.figure(figsize=(fig_w * 2, fig_h * 2))
     # Grid of plots for figures
     gs = gridspec.GridSpec(*shp, height_ratios=[1, 1])
     gs.update(hspace=0.3, wspace=0.4)
-    #data to plot
+    # data to plot
     for idx_chan, chan in enumerate(('HH', 'VV')):
         for idx_proc, proc in enumerate(('', '_desq')):
             raw = inputs[chan + proc]
             raw_par = raw + '_par'
             raw_data = gpf.rawData(raw_par, raw)
-            #Slc parameters
+            # Read range and azimuth indices
+            ridx = params.ref['azidx']
+            azidx = params.ref['azidx']
+            # Slc parameters
             slc = gpf.par_to_dict(inputs.slc_par)
-            #Slice
-            az_slice = raw_data.azimuth_slice_from_slc_idx(params.ref['azidx'], width[1])
-            #Construct
-            raw_sl = raw_data[:, az_slice] * 1
-            #Range filter
-            raw_filt = _sig.hilbert(raw_sl.filter_range_spectrum(slc, params.ref['ridx'], rwin, k=2), axis=0)
+            # Fit squint
+            squint_idx, squint, sq_par, raw_filt = cal.fit_squint(raw_data, slc, params.ref['azidx'], params.ref['ridx'],
+                                                              win=width, z=z)
             ax = f.add_subplot(gs[idx_proc, idx_chan])
-            az_vec = np.arange(-raw_filt.shape[1]//2, raw_filt.shape[1]//2) * raw_data.azspacing
-            squint_idx, win_slice = squint_vec(raw_filt)
-            squint = az_vec[squint_idx[::-1]]
-            #fit squint
-            sq_par = np.polyfit(raw_data.freqvec[win_slice], squint,1)
+            az_vec = np.arange(-raw_filt.shape[1] // 2, raw_filt.shape[1] // 2) * raw_data.azspacing
             squint_fit = raw_data.freqvec * sq_par[0] + sq_par[1]
-            ext_vec = [az_vec[0], az_vec[-1], raw_data.freqvec[0], raw_data.freqvec[-1],]
-            ax.imshow(np.abs(raw_filt), aspect=1e-8, extent= ext_vec)
+            ext_vec = [az_vec[0], az_vec[-1], raw_data.freqvec[0], raw_data.freqvec[-1], ]
+            ax.imshow(np.abs(raw_filt), aspect=1e-8, extent=ext_vec)
             ax.plot(squint_fit, raw_data.freqvec)
-            #Text with fit params
+            # Text with fit params
             bbox_props = dict(boxstyle="square", fc="white", ec="w", lw=2)
-            t = ax.text(0.1, 0.1, "$a=${:.2f} $\\frac{{\circ}}{{GHz}}$".format(sq_par[0]/1e-9), size=8, bbox=bbox_props, horizontalalignment='left',
-                                transform=ax.transAxes)  # set label
+            t = ax.text(0.1, 0.1, "$a=${:.2f} $\\frac{{\circ}}{{GHz}}$".format(sq_par[0] / 1e-9), size=8,
+                        bbox=bbox_props, horizontalalignment='left',
+                        transform=ax.transAxes)  # set label
             if idx_chan == idx_proc == 0:
                 ax.set_xlabel(xlabel)
                 ax.set_ylabel(ylabel)
             fm_func = lambda x, pos: format_freq(raw_data, x, pos)
             fmt = tick.FuncFormatter(fm_func)
             ax.yaxis.set_major_formatter(fmt)
-            #Plot index
-            current_idx = np.ravel_multi_index((idx_chan , idx_proc), shp)
+            # Plot index
+            current_idx = np.ravel_multi_index((idx_chan, idx_proc), shp)
             label_name = string.ascii_lowercase[current_idx]
             # current_ax = axarr[idx_chan, idx_proc]
             ax.title.set_text(r"({label_name})".format(label_name=label_name))
             f.suptitle(params.ref['name'])
             f.subplots_adjust(hspace=0.15, wspace=0.05)
             # ax.xaxis.set_major_locator(tick.MultipleLocator(0.5))
-            f.savefig(outputs['paper_fig'])
-            #New figure to plot subplots
+            plt.show()
+            # New figure to plot subplots
             f1, ax1 = plt.subplots(figsize=(fig_w, fig_h))
             ax1.imshow(np.abs(raw_filt).T, aspect=1e7, extent=ext_vec[::-1])
             ax1.plot(raw_data.freqvec, squint_fit)
             ax1.set_xlabel(ylabel)
             ax1.set_ylabel(r'$\theta_{sq}$ [$^\circ$]')
             f1.savefig(outputs['pres_fig'][current_idx])
-
+        f.savefig(outputs['paper_fig'])
 
 
 plot_figure_12(snakemake.input, snakemake.output, snakemake.threads, snakemake.config, snakemake.params,
-              snakemake.wildcards)
+               snakemake.wildcards)
--- scripts/table_squint.py
+++ scripts/table_squint.py
@@ -1,36 +1,24 @@
-import matplotlib.pyplot as plt
-import matplotlib.ticker as tick
-import numpy as np
-import pyrat.core.corefun as cf
-import pyrat.diff.intfun as _ifun
-import pyrat.fileutils.gpri_files as gpf
-import pyrat.visualization.visfun as vf
-from matplotlib import gridspec
-import matplotlib.ticker as tick
-import string
-import scipy.signal as _sig
-
 import csv
+
+import numpy as np
+
+import pyrat.fileutils.gpri_files as gpf
+from pyrat.gpri_utils import calibration as cal
+
 
 # Return the decimated azimuth position
 def az_idx(ds, idx):
     return idx / ds.azimuth_looks
 
+
 def center_width_from_slice(sl):
-    center = (sl.stop + sl.start)//2
+    center = (sl.stop + sl.start) // 2
     width = (sl.stop - sl.start)
     return center, width
 
 
-def squint_vec(rawdata, z=2000):
-    win_slice = slice(z, rawdata.shape[0] - z)
-    rawdata_sl = rawdata[win_slice,:]#window the edges
-    max_idx = np.argmax(np.abs(rawdata_sl), axis=1)
-    return max_idx, win_slice
-
-
 width = (10, 100)
-rwin = 4
+z = 1000
 
 
 def compute_table_4(inputs, outputs, threads, config, params, wildcards):
@@ -38,25 +26,27 @@ def compute_table_4(inputs, outputs, threads, config, params, wildcards):
     slc = gpf.par_to_dict(inputs.slc_par)
     # header for csv
     header = ['name', 'a_HH', 'a_VV', 'a_res_HH', 'a_res_VV']
-    res = np.zeros((len(params.ref),2*2))
+    res = np.zeros((len(params.ref), 2 * 2))
     for idx_chan, chan in enumerate(('HH', 'VV')):
         for idx_proc, proc in enumerate(('', '_desq')):
             raw = inputs[chan + proc]
             raw_par = raw + '_par'
             raw_data = gpf.rawData(raw_par, raw)
-            idx_res = np.ravel_multi_index((idx_proc, idx_chan), (2,2))
+            idx_res = np.ravel_multi_index((idx_proc, idx_chan), (2, 2))
             for idx_ref, ref in enumerate(params.ref):
-                #Slice
-                az_slice = raw_data.azimuth_slice_from_slc_idx(ref['azidx'], width[1])
-                #Construct
-                raw_sl = raw_data[:, az_slice] * 1
-                #Range filter
-                raw_filt = _sig.hilbert(raw_sl.filter_range_spectrum(slc, ref['ridx'], rwin, k=2), axis=0)
-                az_vec = np.arange(-raw_filt.shape[1]//2, raw_filt.shape[1]//2) * raw_data.azspacing
-                squint_idx, win_slice = squint_vec(raw_filt)
-                squint = az_vec[squint_idx[::-1]]
-                #fit squint
-                sq_par = np.polyfit(raw_data.freqvec[win_slice], squint,1)
+                squint_idx, squint, sq_par, raw_filt = cal.fit_squint(raw_data, slc, params.ref['azidx'],
+                                                                  params.ref['ridx'], win=width, z=z)
+                # #Slice
+                # az_slice = raw_data.azimuth_slice_from_slc_idx(ref['azidx'], width[1])
+                # #Construct
+                # raw_sl = raw_data[:, az_slice] * 1
+                # #Range filter
+                # raw_filt = _sig.hilbert(raw_sl.filter_range_spectrum(slc, ref['ridx'], rwin, k=2), axis=0)
+                # az_vec = np.arange(-raw_filt.shape[1]//2, raw_filt.shape[1]//2) * raw_data.azspacing
+                # squint_idx, win_slice = squint_vec(raw_filt)
+                # squint = az_vec[squint_idx[::-1]]
+                # #fit squint
+                # sq_par = np.polyfit(raw_data.freqvec[win_slice], squint,1)
                 res[idx_ref, idx_res] = sq_par[0]
     names = [ref['name'] for ref in params.ref]
     with open(outputs[0], 'w+') as of:
@@ -72,7 +62,5 @@ def compute_table_4(inputs, outputs, threads, config, params, wildcards):
     print(res)
 
 
-
-
 compute_table_4(snakemake.input, snakemake.output, snakemake.threads, snakemake.config, snakemake.params,
-              snakemake.wildcards)
+                snakemake.wildcards)
