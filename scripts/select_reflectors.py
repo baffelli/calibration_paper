57a7879f585c43156801fcf3a63c4a46666595cd
baffelli <baffelli@ifu.baug.ethz.ch>
2017-08-24 14:29:35 +0200

    Added utility function to select reflector from image.

--- scripts/select_reflectors.py
+++ scripts/select_reflectors.py
@@ -21,88 +21,56 @@ import pyrat.fileutils.gpri_files as gpf
 import collections
 from scipy.special import expit
 
-def annotate_axis(ax, params):
-    # box = dict(boxstyle='round', fc='white', alpha=1)
-    pos_list = collections.defaultdict(lambda:(30,0))
-    pos_list.update( {'CR1': (30, 10), 'CR3':(20,0), 'CR4':(30,-10),'CR6': (10,-20)})
-    for ref in params['ref']:#iterate reflector and positions
-        ref_pos = ref['geo_coord']
-        ax.plot(ref_pos[0], ref_pos[1], mec=ref['marker_color'], marker='o', mfc='none', ms=10)
-        ax.annotate(
-            ref['name'],
-            xy = ref_pos, xytext = pos_list[ref['name']],
-            textcoords = 'offset points', ha = 'right', va = 'bottom', color='white')
+import pyrat.geo.utils as ut
 
+import pyrat.geo.transforms as trasf
 
-def plot_figure_7(inputs, outputs, threads, config, params, wildcards):
-    #load data
-    C_cal_par = inputs["C_cal_par"]
-    C_cal_root, ext = path.splitext(C_cal_par)
-    C_cal = mat.coherencyMatrix(C_cal_root, C_cal_par, gamma=True, bistatic=True,
-                                basis='lexicographic').lexicographic_to_pauli()
-
-    #Load shadow map
-    sh_map = gpf.gammaDataset(inputs.dem_seg_par, inputs.sh_map, dtype=gpf.type_mapping['UCHAR'])
-
+def select_reflectors(inputs, outputs, threads, config, params, wildcards):
     #load map
     map_ds = gdal.Open(inputs.map)
-    #Create spatial reference
-    ref = osr.SpatialReference()
-    ref.ImportFromWkt(map_ds.GetProjection())
-
-    #create figure
-    plt.style.use(inputs['style'])
-    fig_w, fig_h = plt.rcParams['figure.figsize']
-    f, ax = plt.subplots(figsize=(fig_w, fig_h*2))
-
-    #Geocode
-    LUT = geo.GeocodingTable(inputs.dem_seg_par, inputs.LUT)
-
-    #Decimate reflector location
-    for ref in params['ref']:
-        ref['azidx_dec'] = C_cal.azidx_dec(ref['azidx'])
-        ref['geo_coord'] = LUT.dem_coord_to_geo_coord(LUT.radar_coord_to_dem_coord([ref['ridx'], ref['azidx_dec']]))
-        ref['marker_color'] = 'orange' if ref['name'] == params['ref'][config['calibration']['reflector_index']]['name'] else '#43a2ca'
-    print(params['ref'])
-
-    #Geocode
-    C_cal_geo = LUT.geocode_data(C_cal)
-    C_cal_geo = C_cal_geo.boxcar_filter([3,3])
-    C_cal_rgb = C_cal_geo.pauli_image(k=0.4, sf=0.25, common=False, peak=False)
-    C_cal_alpha = np.ones(C_cal_rgb[:,:,0].shape)
-    #Mask Nans
-    C_cal_span = C_cal_geo.span()
-    C_cal_alpha = np.ones_like(C_cal_span)
-    # C_cal_alpha[(C_cal_geo.span()/np.nanmax(C_cal_span)) < 1e-6] = 0
-    C_cal_alpha[np.isnan(C_cal_geo[:,:,0,0])] = 0
-    #Convert to rgba
-    C_cal_rgb = np.dstack((C_cal_rgb,C_cal_alpha))
-    #Plot map
+    dem_seg = gdal.Open(inputs.dem_seg)
+    #Load geocoding table
+    LUT = geo.GeocodingTable(inputs.dem_seg_par, inputs.LUT, inputs.ref_mli_par, inputs.LUT1)
     ext1=geo.get_ds_extent(map_ds)
-    ext2=LUT.get_extent()
+    or_pt = [1500,300]
+    pt1 =LUT.radar_coord_to_geo_coord(or_pt)
+
+    f, ax = plt.subplots()
     ax.imshow(map_ds.ReadAsArray().transpose((1, 2, 0)), extent=ext1)
-    ax.imshow(C_cal_rgb.transpose(1,0,2), extent=ext2)
-    # ax.imshow(sh_map.transpose((1, 0)
-    #                            ), extent=LUT.get_extent())
-    #annotate scatters
-    annotate_axis(ax, params)
-    ext = LUT.get_geocoded_extent(C_cal)
-    ax.set_xlim([606637,609660])
-    ax.set_ylim(ext[2:])
-    ax.set_aspect(1)
-    # f.subplots_adjust(left=0,right=1)
-    ax.axis('off')
-    f.savefig(outputs['paper_fig'], bbox_inches='tight', pad_inches=0)
-    #Add axis
-    f1, ax1 =  plt.subplots(figsize=( fig_w, fig_w))
-    ax1.imshow(C_cal_rgb.transpose(1, 0, 2), extent=ext2, aspect=1)
-    ax1.axis('off')
-    # annotate scatters
-    annotate_axis(ax1, params)
-    f1.subplots_adjust(left=0, right=1)
-    f1.savefig(outputs['pres_fig'])
+    ax.imshow(dem_seg.ReadAsArray().real, extent=geo.get_ds_extent(dem_seg))
+    ax.plot(pt1[:,0], pt1[:,1], marker='o')
+    plt.show()
+    # print(dir(LUT.radar_idx_to_dem_idx_t))
+    # radar_idx = LUT.radar_coord_to_geo_coord([10,10])
+    # print(radar_idx)
+    # #Get geo transform
+    # gt = geo.get_geotransform(gpf.par_to_dict(inputs.dem_seg_par))
+    # # a = trasf.LutAxes(gt=gt, lut=np.array(LUT))
+    # # a.imshow(np.abs(slc))
+    # f = plt.axes(projection=trasf.LutProjection(gt=gt, lut=np.array(LUT)))
+
+
+    # LUT = geo.GeocodingTable(inputs.dem_seg_par, inputs.LUT)
+    # plt.subplot(2,1,1)
+    # plt.imshow(np.real(LUT.lut.lut))
+    # plt.subplot(2, 1, 2)
+    # plt.imshow(np.real(LUT.inverse_lut.lut))
+    # plt.show()
+    # pt = [2500,363]
+    # radar_index = LUT.dem_index_to_radar_index(pt)
+    # print(radar_index)
+    # dem_index = LUT.radar_index_to_dem_index(radar_index[0])
+    # print(dem_index,pt)
+    # #load map
+    # map_ds = gdal.Open(inputs.map)
+    # #Create spatial reference
+    # ref = osr.SpatialReference()
+    # ref.ImportFromWkt(map_ds.GetProjection())
+    # ut.GeocodedVisualization(slc, LUT, map_ds)
+    # plt.show()
 
 
 
-plot_figure_7(snakemake.input, snakemake.output, snakemake.threads, snakemake.config, snakemake.params,
+
+select_reflectors(snakemake.input, snakemake.output, snakemake.threads, snakemake.config, snakemake.params,
               snakemake.wildcards)
